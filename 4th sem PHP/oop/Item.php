c420a7bd54c828bac974eb9a466fd0df294d2384
Nikesh Adhikari <110375186+ultimatum254@users.noreply.github.com>
2023-05-31 09:40:30 +0545

    started oop and constructor, method and properties made

---
+++ 4th sem PHP/oop/Item.php
@@ -0,0 +1,17 @@
+<?php
+
+class Item{
+
+    public $name = 'HELLO';
+    public $description;
+
+    // function sayHello()
+    // {
+    //     echo "hello programmerss<br>";
+    // }
+
+    function __construct()
+    {
+        echo "constructor called<br>";
+    }
+}
---
+++ 4th sem PHP/oop/example.php
@@ -0,0 +1,9 @@
+<?php
+require './Item.php';
+
+$first_obj = new Item();
+
+// $first_obj->name = "new name";
+// $first_obj->sayHello();
+
+?>
