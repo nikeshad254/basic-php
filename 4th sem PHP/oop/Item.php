35816c0f5cb495cda2af76ca81d47df1893b8da9
Nikesh Adhikari <110375186+ultimatum254@users.noreply.github.com>
2023-06-01 06:33:50 +0545

    inheritance of php oop

--- 4th sem PHP/oop/Item.php
+++ 4th sem PHP/oop/Item.php
@@ -1,16 +1,10 @@
 <?php
 
 class Item{
+
     public $name;
-    public $address;
-    public static $count=0;
-    public function __construct($name, $address)
-    {
-        $this->name = $name;
-        $this->address=$address;
-        static::$count++;
-    }
-    public static function showCount(){
-        echo static::$count;
+    
+    public function getDescription(){
+        return $this->name;
     }
 }
--- 4th sem PHP/oop/example.php
+++ 4th sem PHP/oop/example.php
@@ -1,15 +1,19 @@
 <?php
 require './Item.php';
+require './Book.php';
 
-// static access garna lai, class name and clos :: needed
-var_dump(Item::$count);
-$first_obj = new Item('raju ', 'balaju');
-var_dump(Item::$count);
-echo $first_obj->name;
+$first_obj = new Item();
+$first_obj->name = "TV";
 
-$second_obj = new Item('hari ', 'jamal');
-var_dump(Item::$count);
-echo $second_obj->name;
-Item::showCount();
+echo $first_obj->getDescription();
+
+echo "<br>";
+
+$book = new Book();
+$book -> name = "web dev";
+$book -> author = " raja ram";
+
+echo $book->getDescription();
+echo $book->author;
 
 ?>
