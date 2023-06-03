4ebc1175f7c9d30ac839a9569cab930bdb842277
Nikesh Adhikari <110375186+ultimatum254@users.noreply.github.com>
2023-06-03 20:22:13 +0545

    index now implements fetching all data and calculations

--- 4th sem PHP/srms/index.php
+++ 4th sem PHP/srms/index.php
@@ -3,6 +3,48 @@
 require './includes/header.php';
 require './includes/database.php';
 
+$conn = getDB();
+$sql = "select * from student";
+$result = mysqli_query($conn, $sql);
+if ($result === false) {
+    echo mysqli_error($conn);
+} else {
+    $students = mysqli_fetch_all($result,  MYSQLI_ASSOC);
+
+    // echo '<pre>';
+    // print_r($students);
+    // exit;
+}
+
+function passFail($math, $science, $english, $social, $nepali){
+    if($math>=40 && $science>=40 && $english>=40 && $social>=40 && $nepali>=40){
+        return "PASS";
+    }else{
+        return "FAIL";
+    }
+}
+
+function getDivision($math, $science, $english, $social, $nepali){
+    if(passFail($math, $science, $english, $social, $nepali) == "FAIL"){
+        return "#";
+    }
+    else{
+        $percentage = ($math + $science + $english + $social + $nepali) / (5 * 100) * 100;
+        $division = $percentage >= 90 ? "DISTINCTION" : ($percentage >= 80 ? "FIRST" : ($percentage >= 60 ? "SECOND" : ($percentage >= 40 ? "THIRD" : "#")));
+        return $division;
+    }
+}
+
+function getGPA($math, $science, $english, $social, $nepali){
+    if(passFail($math, $science, $english, $social, $nepali) == "FAIL"){
+        return "#";
+    }
+    else{
+        $percentage = ($math + $science + $english + $social + $nepali) / (5 * 100) * 100;
+        return $percentage/100*4;
+    }
+}
+
 ?>
 
 <a href="./addStu.php">Add Student</a>
@@ -22,19 +64,27 @@ require './includes/database.php';
         <th>GPA</th>
     </thead>
     <tbody>
+        <?php if (empty($students)) : ?>
+            <p>no students data ... </p>
+        <?php else : ?>
+
+            <?php $count = 1; 
+                foreach ($students as $student) :?>
         <tr>
-            <td>1</td>
-            <td><a href="#">name</a></td>
-            <td>Phone</td>
-            <td>Matd</td>
-            <td>Science</td>
-            <td>English</td>
-            <td>Social</td>
-            <td>Nepali</td>
-            <td>Remark</td>
-            <td>Division</td>
-            <td>GPA</td>
+            <td><?= $count ?></td>
+            <td><a href="#"><?= $student['name']?></a></td>
+            <td><?= $student['phone']?></td>
+            <td><?= $student['math']?></td>
+            <td><?= $student['science']?></td>
+            <td><?= $student['social']?></td>
+            <td><?= $student['english']?></td>
+            <td><?= $student['nepali']?></td>
+            <td><?= passFail($student['math'], $student['science'], $student['social'], $student['english'], $student['nepali']); ?></td>
+            <td><?= getDivision($student['math'], $student['science'], $student['social'], $student['english'], $student['nepali']); ?></td>
+            <td><?= getGPA($student['math'], $student['science'], $student['social'], $student['english'], $student['nepali']); ?></td>
         </tr>
+            <?php $count++; endforeach; ?>
+        <?php endif;?>
     </tbody>
 </table>
 
